9b0584c15b5b94cfd8462cd0773b1c5fb352ab5b
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-09-25 22:06:19 +0530

    authentication

---
+++ Backend/index.js
@@ -0,0 +1,34 @@
+import express, { application } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+// to connect our application to mongodb
+const connect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO)
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+//middlewares
+app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use("/api/authentication",userRoutes);
+
+
+app.listen(process.env.PORT || 5000, () => {
+    connect();
+    console.log("Connected to Server");
+  });
